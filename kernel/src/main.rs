73e9bb7269e8c43b0b2a21d8e2c51e738c0f6c9a
David Hoppenbrouwers <lemmings@demindiro.com>
2025-10-16 22:18:52 +0200

    kernel: move `mod sys` to sys.rs

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,62 +1,9 @@
 #![no_std]
 #![no_main]
 
+mod sys;
+
 use core::arch::asm;
-
-mod sys {
-    pub static mut ENTRY: *const () = core::ptr::null();
-
-    macro_rules! sys {
-        ($nr:literal ["rsi" $val:expr] ["rcx" $val2:expr]) => {{
-            let ret: isize;
-            core::arch::asm! {
-                "call [rip + {}]",
-                sym ENTRY,
-                in("eax") $nr,
-                // spec claims it accepts a string literal token
-                // but rustc disagrees, so do the stupid thing instead :)
-                //
-                // error: expected register class or explicit register
-                //   --> src/main.rs:17:22
-                //    |
-                // 17 |                 $(in($reg) $val,)*
-                //    |                      ^^^^
-                // ...
-                // 29 |         unsafe { sys!(1 ["rsi" s.as_ptr()] ["rcx" s.len()]) }
-                //    |                  ------------------------------------------ in this macro invocation
-                //    |
-                //    = note: this error originates in the macro `sys` (in Nightly builds, run with -Z macro-backtrace for more info)
-                in("rsi") $val,
-                in("rcx") $val2,
-                lateout("rax") ret,
-                lateout("rcx") _,
-                lateout("rdx") _,
-                lateout("rsi") _,
-                lateout("rdi") _,
-            };
-            ret
-        }};
-        (noreturn $nr:literal ["rdx" $val:expr]) => {{
-            let ret: isize;
-            core::arch::asm! {
-                "call [rip + {}]",
-                sym ENTRY,
-                in("eax") $nr,
-                in("rdx") $val,
-                options(noreturn)
-            };
-            ret
-        }};
-    }
-
-    pub fn print(s: &str) {
-        unsafe { sys!(0 ["rsi" s.as_ptr()] ["rcx" s.len()]); }
-    }
-
-    pub fn exit_ok() -> ! {
-        unsafe { sys!(noreturn 1 ["rdx" 0]); }
-    }
-}
 
 fn fail(reason: &str) -> ! {
     sys::print(reason);
