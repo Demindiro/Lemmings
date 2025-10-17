57af3a856c41b997b35efa792e98618060ab3835
David Hoppenbrouwers <lemmings@demindiro.com>
2025-10-17 20:53:08 +0200

    qemubios: use custom target spec with no-PIE output

--- qemubios/build.sh
+++ qemubios/build.sh
@@ -5,9 +5,17 @@ export RUSTC_BOOTSTRAP=1
 
 set -xe
 
+TARGET=x86_64-qemubios
+RUST_TARGET="$RUST_OUT/$TARGET"
+TARGET_SPEC="$PWD/$TARGET.json"
+
+for pkg in core compiler_builtins rustc-std-workspace-core
+do
+	(cd "$RUST_SRC" && cargo b --release --package $pkg --target "$TARGET_SPEC" $opt)
+done
+
 rustc --edition=2024 --crate-type=lib \
-	--target ../$TARGET.json \
-	-Cpanic=abort \
+	--target "$TARGET_SPEC" \
 	-L"$RUST_TARGET/release" \
 	-O \
 	qemubios.rs -o /tmp/qemubios-rs.o
