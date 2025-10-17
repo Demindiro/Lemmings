35a8c9d2c551309ed4ae6f94d11af055c7f93631
David Hoppenbrouwers <lemmings@demindiro.com>
2025-10-17 21:20:59 +0200

    kernel: add critical_section

---
+++ kernel/src/critical_section.rs
@@ -0,0 +1,32 @@
+use core::arch::asm;
+use critical_section::RawRestoreState;
+
+struct Impl;
+critical_section::set_impl!(Impl);
+
+unsafe impl critical_section::Impl for Impl {
+    unsafe fn acquire() -> RawRestoreState {
+        let token;
+        unsafe {
+            asm! {
+                "pushf",
+                "pop {0:r}",
+                "cli",
+                out(reg) token,
+                options(nomem, preserves_flags),
+            }
+        }
+        token
+    }
+
+    unsafe fn release(token: RawRestoreState) {
+        unsafe {
+            asm! {
+                "push {0:r}",
+                "popf",
+                in(reg) token,
+                options(nomem, preserves_flags),
+            }
+        }
+    }
+}
