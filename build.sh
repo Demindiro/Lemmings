123b0f6322804d98ca2beb723da0d744238806a2
David Hoppenbrouwers <lemmings@demindiro.com>
2025-10-20 04:07:47 +0200

    build: remove create_databin.sh, specify env variables for data.bin output

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 set -xe
+
+. ./config.env
+export tmp="/tmp/lemmings.tmp"
+export out="/tmp"
+export databin="$out/data.bin.dir"
+
+rm -rf "$databin"
+mkdir -p "$databin"
+mkdir -p "$tmp"
+
 (cd qemubios && ./build.sh)
 (cd kernel && ./build.sh)
-./create_databin.sh
+
+./create_archive.py "$out/data.bin" "$databin"
