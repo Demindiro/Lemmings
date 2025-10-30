9de9a303da17af2af0feea04c51025b05ceb8150
David Hoppenbrouwers <lemmings@demindiro.com>
2025-10-30 21:19:53 +0100

    lib/x86_64: make apic::local_address safe
    Realistically, getting the address only should never cause ill effects.
    
    Any long-mode processor is guaranteed to have a APIC.

--- lib/x86_64/src/apic/mod.rs
+++ lib/x86_64/src/apic/mod.rs
@@ -7,7 +7,7 @@ use x86::msr;
 
 const IA32_APIC_BASE_MSR_ENABLE: u64 = 1 << 11;
 
-pub unsafe fn local_address() -> mmu::Phys<mmu::A12> {
+pub fn local_address() -> mmu::Phys<mmu::A12> {
     let base = unsafe { msr::rdmsr(msr::IA32_APIC_BASE) };
     mmu::Phys::new_masked(base)
 }
