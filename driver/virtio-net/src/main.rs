f5dec34afc0ae1fbcb0f1dcc7227ca53e0ea778d
David Hoppenbrouwers <lemmings@demindiro.com>
2025-11-03 22:04:38 +0100

    driver/virtio-net: look for PCI door
    Doesn't work, of course

--- driver/virtio-net/src/main.rs
+++ driver/virtio-net/src/main.rs
@@ -4,8 +4,15 @@
 
 extern crate lemmings_door;
 
+use lemmings_door::{ApiId, Cookie};
+use lemmings_idl_pci::Pci;
+
 fn main() {
-    todo!();
+    let door =
+        lemmings_door::door_list(Some(ApiId(Pci::ID)), Cookie(0)).expect("no PCI door found");
+    let door =
+        lemmings_door::door_list(Some(ApiId(Pci::ID)), Cookie(0)).expect("no PCI door found");
+    todo!("{door:?}");
 }
 
 #[unsafe(no_mangle)]
