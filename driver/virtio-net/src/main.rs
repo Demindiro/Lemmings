fe57e597f537e67cdd5483ec951e21e4b0de90e8
David Hoppenbrouwers <lemmings@demindiro.com>
2025-11-07 18:26:39 +0100

    driver/virtio-net: iterate over PCI devices

--- driver/virtio-net/src/main.rs
+++ driver/virtio-net/src/main.rs
@@ -4,12 +4,26 @@
 
 extern crate lemmings_door;
 
-use lemmings_door::{ApiId, Cookie};
+use lemmings_door::{ApiId, Cookie, Log};
 use lemmings_idl_pci::Pci;
 
 fn main() {
-    let door = lemmings_door::door_find::<Pci>(Cookie(0)).expect("no PCI door found");
-    todo!("{door:?}");
+    let (door, _) = lemmings_door::door_find::<Pci>(Cookie(0)).expect("no PCI door found");
+    let door = door.get();
+    let lemmings_idl_pci::Configuration {
+        base,
+        segment_group,
+        bus_start,
+        bus_end,
+    } = door.configuration();
+    assert!(u8::from(bus_start) == 0 && u8::from(bus_end) == 0xff);
+    let pci = unsafe { base.0.cast::<lemmings_pci::Pci>().as_ref() };
+    pci.list(|bdf, hdr| {
+        use core::fmt::Write;
+        let vendor = hdr.vendor_id();
+        let device = hdr.device_id();
+        let _ = writeln!(Log::new(), "{bdf:?} -> {vendor:04x}:{device:04x}");
+    });
 }
 
 #[unsafe(no_mangle)]
