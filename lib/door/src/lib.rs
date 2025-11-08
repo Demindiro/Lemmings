5338c1723e7f738061a43bf8e9608c8f4df15990
David Hoppenbrouwers <lemmings@demindiro.com>
2025-11-08 18:41:50 +0100

    lib/door: add log!() and dbg!()

--- lib/door/src/lib.rs
+++ lib/door/src/lib.rs
@@ -9,6 +9,46 @@ use core::{
     num::NonZero,
 };
 
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {{
+        use core::fmt::Write;
+        let _ = write!(Log::new(), $($arg)*);
+    }};
+}
+
+#[macro_export]
+macro_rules! dbg {
+    // NOTE: We cannot use `concat!` to make a static string as a format argument
+    // of `eprintln!` because `file!` could contain a `{` or
+    // `$val` expression could be a block (`{ .. }`), in which case the `eprintln!`
+    // will be malformed.
+    () => {
+        $crate::log!("[{}:{}:{}]", file!(), line!(), column!())
+    };
+    ($val:expr $(,)?) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                $crate::log!("[{}:{}:{}] {} = {:#?}",
+                    file!(),
+                    line!(),
+                    column!(),
+                    stringify!($val),
+                    // The `&T: Debug` check happens here (not in the format literal desugaring)
+                    // to avoid format literal related messages and suggestions.
+                    &&tmp as &dyn core::fmt::Debug,
+                );
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
+
 #[cfg(target_arch = "x86_64")]
 mod ffi {
     use core::arch::asm;
