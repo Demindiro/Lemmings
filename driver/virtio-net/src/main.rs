c50981bd1c4e158bc3e4a9e855231c62c9656c9c
David Hoppenbrouwers <lemmings@demindiro.com>
2025-11-08 01:03:32 +0100

    driver/virtio-net: integrate lib/virtio-net
    For whatever reason I'm getting a page fault however.
    
    error code: 0x000b, meaning Present, Write and Reserved Write.
    
    Why reserved write? All bits should be valid...
    QEMU also lists the address range as mapped. Truly strange...

--- driver/virtio-net/src/main.rs
+++ driver/virtio-net/src/main.rs
@@ -4,8 +4,13 @@
 
 extern crate lemmings_door;
 
+use core::ptr::NonNull;
 use lemmings_door::{ApiId, Cookie, Log};
 use lemmings_idl_pci::Pci;
+use lemmings_pci::{Header, Header0, ParsedBaseAddress};
+
+const VENDOR_ID: u16 = 0x1af4;
+const DEVICE_ID: u16 = 0x1000;
 
 fn main() {
     let (door, _) = lemmings_door::door_find::<Pci>(Cookie(0)).expect("no PCI door found");
@@ -18,12 +23,54 @@ fn main() {
     } = door.configuration();
     assert!(u8::from(bus_start) == 0 && u8::from(bus_end) == 0xff);
     let pci = unsafe { base.0.cast::<lemmings_pci::Pci>().as_ref() };
-    pci.list(|bdf, hdr| {
-        use core::fmt::Write;
-        let vendor = hdr.vendor_id();
-        let device = hdr.device_id();
-        let _ = writeln!(Log::new(), "{bdf:?} -> {vendor:04x}:{device:04x}");
-    });
+    pci.list(|_, hdr| try_device(hdr));
+}
+
+fn try_device(header: Header<'_>) {
+    let Header::H0(hdr) = header else {
+        return;
+    };
+    if hdr.vendor_id() != VENDOR_ID {
+        return;
+    }
+    if hdr.device_id() != DEVICE_ID {
+        return;
+    }
+    start_device(hdr);
+}
+
+fn start_device(header: &Header0) -> ! {
+    let msix = lemmings_virtio_net::Msix {
+        receive_queue: Some(0),
+        transmit_queue: Some(1),
+    };
+    let bar_map = &map_bars(header);
+    let dma_alloc = |x| -> Result<_, ()> {
+        todo!();
+    };
+    let dev = unsafe { lemmings_virtio_net::Device::new(header, msix, bar_map, dma_alloc) };
+    todo!("Use the device, Luke!");
+}
+
+fn map_bars(header: &Header0) -> [Option<NonNull<u8>>; 6] {
+    let mut i = 0;
+    let mut map = [None; 6];
+    while i < 6 {
+        match header.full_base_address(i) {
+            None | Some(ParsedBaseAddress::IO32 { .. }) => i += 1,
+            Some(ParsedBaseAddress::MMIO32 { address, .. }) => {
+                map[i] = NonNull::new(address as _);
+                i += 1;
+            }
+            Some(ParsedBaseAddress::MMIO64 { address, .. }) => {
+                map[i] = NonNull::new(address as _);
+                i += 2;
+            }
+        }
+    }
+    use core::fmt::Write;
+    let _ = write!(Log::new(), "{map:?}");
+    map
 }
 
 #[unsafe(no_mangle)]
