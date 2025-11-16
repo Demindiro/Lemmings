f5fc32f39f2c39184e8007e0bf515fbb3c44df03
David Hoppenbrouwers <lemmings@demindiro.com>
2025-11-16 21:36:44 +0100

    kernel: fix incorrect ref to TCB in set_current instead of Thread

--- kernel/src/thread.rs
+++ kernel/src/thread.rs
@@ -380,13 +380,13 @@ impl fmt::Debug for ThreadSpawnError {
 }
 
 unsafe fn set_current(thread: &Thread) {
-    unsafe { lemmings_x86_64::set_fs(&thread.tcb as *const _ as *mut u8) }
+    unsafe { lemmings_x86_64::set_fs(thread as *const _ as *mut u8) }
 }
 
 pub fn current() -> ThreadHandle {
-    let tcb = lemmings_x86_64::fs().cast::<Thread>();
-    let tcb = NonNull::new(tcb).expect("fs register should not be null");
-    unsafe { ThreadHandle(ThreadRef::wrap(tcb)) }
+    let t = lemmings_x86_64::fs().cast::<Thread>();
+    let t = NonNull::new(t).expect("fs register should not be null");
+    unsafe { ThreadHandle(ThreadRef::wrap(t)) }
 }
 
 /// Spawn a new thread and run it immediately.
