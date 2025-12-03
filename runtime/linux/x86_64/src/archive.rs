87f6a70be829e17fa1b8ea06488c26bb8ee2c01a
David Hoppenbrouwers <lemmings@demindiro.com>
2025-12-03 16:04:09 +0100

    runtime/linux: archive: mmap files

--- runtime/linux/x86_64/src/archive.rs
+++ runtime/linux/x86_64/src/archive.rs
@@ -6,7 +6,7 @@
 // (we also get to avoid read() calls, yay!)
 
 use crate::{
-    linux::{CStr, LinuxDirent64},
+    linux::{CStr, LinuxDirent64, Stat},
     syscall,
 };
 use core::{
@@ -19,8 +19,20 @@ use core::{
 
 static mut ROOT: MaybeUninit<Dir> = MaybeUninit::uninit();
 
-struct Dir {
+#[derive(Clone, Copy)]
+pub enum Item {
+    File(File),
+    Dir(Dir),
+}
+
+#[derive(Clone, Copy)]
+pub struct Dir {
     base: NonNull<u8>,
+}
+
+#[derive(Clone, Copy)]
+pub struct File {
+    data: &'static [u8],
 }
 
 #[derive(Debug)]
@@ -47,11 +59,21 @@ struct IterDir<'a> {
 }
 
 impl Dir {
+    pub fn find(&self, name: &str) -> Option<Item> {
+        self.entries()
+            .find(|x| x.name == name)
+            .map(|x| match x.is_dir {
+                true => Item::Dir(Dir { base: x.child_buf }),
+                false => Item::File(File {
+                    data: unsafe { slice::from_raw_parts(x.child_buf.as_ptr(), x.child_len) },
+                }),
+            })
+    }
+
     fn entries(&self) -> impl Iterator<Item = DirEntry<'_>> + '_ {
         let (entries, names) = self.raw_entries();
-        entries.into_iter().map(move |e| unsafe {
-            let name = unsafe { names.add(e.name_offset as usize).as_ptr() };
-            let name = unsafe { slice::from_raw_parts(name, e.name_len.into()) };
+        entries.into_iter().map(move |e| {
+            let name = unsafe { e.name(names) };
             let name = str::from_utf8(name).expect("UTF-8");
             DirEntry {
                 child_buf: e.child_buf,
@@ -63,11 +85,38 @@ impl Dir {
     }
 
     fn raw_entries(&self) -> (&[RawDirEntry], NonNull<u8>) {
-        let num = unsafe { self.base.cast::<usize>().read() };
-        let base = unsafe { self.base.cast::<usize>().add(1) };
-        let entries = unsafe { slice::from_raw_parts(base.cast().as_ptr(), num) };
+        let (num, base) = self.len_entries();
+        let entries = unsafe { slice::from_raw_parts(base.as_ptr(), num) };
         let names = unsafe { base.cast::<RawDirEntry>().add(num).cast() };
         (entries, names)
+    }
+
+    fn raw_entries_mut(&mut self) -> (&mut [RawDirEntry], NonNull<u8>) {
+        let (num, base) = self.len_entries();
+        let entries = unsafe { slice::from_raw_parts_mut(base.as_ptr(), num) };
+        let names = unsafe { base.cast::<RawDirEntry>().add(num).cast() };
+        (entries, names)
+    }
+
+    fn len_entries(&self) -> (usize, NonNull<RawDirEntry>) {
+        let num = unsafe { self.base.cast::<usize>().read() };
+        let base = unsafe { self.base.cast::<usize>().add(1) };
+        (num, base.cast())
+    }
+}
+
+impl RawDirEntry {
+    unsafe fn name(&self, names: NonNull<u8>) -> &[u8] {
+        unsafe {
+            let name = names.add(self.name_offset as usize).as_ptr();
+            slice::from_raw_parts(name, self.name_len.into())
+        }
+    }
+}
+
+impl File {
+    pub fn data(&self) -> &'static [u8] {
+        self.data
     }
 }
 
@@ -96,13 +145,16 @@ impl fmt::Debug for Dir {
     }
 }
 
+pub fn root() -> Dir {
+    unsafe { ROOT.assume_init() }
+}
+
 pub unsafe fn init() {
     let dirfd = unsafe { syscall::open(".\0".as_ptr(), syscall::O_RDONLY, 0) };
     assert!(dirfd >= 0, "failed to open root at \".\" (error {dirfd})");
     let dirent = &mut [const { MaybeUninit::uninit() }; 1024];
     let root = unsafe { collect_dir(dirfd, dirent) };
     unsafe { syscall::close(dirfd) };
-    dbg!(&root);
     unsafe {
         (&mut *(&raw mut ROOT)).write(root);
     }
@@ -156,42 +208,55 @@ unsafe fn collect_dir<const N: usize>(dirfd: i32, dirent: &mut [MaybeUninit<usiz
             p = p.byte_add(name.len());
         }
     }
-    let dir = Dir { base: buf };
+    let mut dir = Dir { base: buf };
 
-    for x in dir.entries() {
-        const { assert!(N * mem::size_of::<usize>() > 128) };
+    let (entries, names) = dir.raw_entries_mut();
+    for x in entries {
+        const { assert!(N * mem::size_of::<usize>() > 256) };
         let mut name = NonNull::from(&mut *dirent).cast::<u8>();
+        let xname = unsafe { x.name(names) };
         unsafe {
-            name.copy_from_nonoverlapping(NonNull::from(x.name).cast(), x.name.len());
-            name.byte_add(x.name.len()).write(0);
+            name.copy_from_nonoverlapping(NonNull::from(xname).cast(), xname.len());
+            name.byte_add(xname.len()).write(0);
         }
         let fd = unsafe { syscall::openat(dirfd, name.as_ptr(), syscall::O_RDONLY, 0) };
-        if x.is_dir {
-            unsafe { collect_dir(fd, dirent) };
+        assert!(fd >= 0, "failed to open entry (error {fd})");
+        (x.child_buf, x.child_len) = if x.is_dir {
+            let Dir { base } = unsafe { collect_dir(fd, dirent) };
+            (base, 0)
         } else {
-            unsafe { map_file(fd) };
-        }
+            let s = unsafe { map_file(fd) };
+            (NonNull::from(s).cast(), s.len())
+        };
         unsafe { syscall::close(fd) };
     }
 
     dir
 }
 
-unsafe fn map_file(fd: i32) -> &'static [u8] {
-    todo!()
+fn map_file(fd: i32) -> &'static [u8] {
+    let stat = &mut Stat::default();
+    let res = unsafe { syscall::fstat(fd, stat) };
+    assert!(res >= 0, "failed to stat fd (error {res})");
+    let len = usize::try_from(stat.size).expect("file too large to memory map");
+    let ptr = memmap(len, fd, false).expect("file memory map failed");
+    unsafe { slice::from_raw_parts(ptr.as_ptr(), len) }
 }
 
 fn alloc(len: usize) -> NonNull<u8> {
-    let ptr = unsafe {
-        syscall::mmap(
-            ptr::null_mut(),
-            len,
-            syscall::PROT_RW,
-            syscall::MAP_ANON | syscall::MAP_PRIVATE,
-            0,
-            0,
-        )
+    memmap(len, -1, true).expect("allocation failed")
+}
+
+fn memmap(len: usize, fd: i32, write: bool) -> Result<NonNull<u8>, i32> {
+    let prot = if write {
+        syscall::PROT_RW
+    } else {
+        syscall::PROT_R
     };
-    // FIXME we need to check -4095..0, not just null
-    NonNull::new(ptr).expect("failed to allocate memory")
+    let flags = if fd < 0 { syscall::MAP_ANON } else { 0 } | syscall::MAP_PRIVATE;
+    let ptr = unsafe { syscall::mmap(ptr::null_mut(), len, prot, flags, fd, 0) };
+    match ptr as isize {
+        x @ -4095..0 => Err(x as i32),
+        _ => NonNull::new(ptr).ok_or(0),
+    }
 }
