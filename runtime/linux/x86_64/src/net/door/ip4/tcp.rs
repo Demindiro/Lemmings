72b69a6ce1466c78a1442eeddc97904029f7dd8a
David Hoppenbrouwers <lemmings@demindiro.com>
2025-12-06 14:57:23 +0100

    runtime/linux: impl IPv4 TCP listen

--- runtime/linux/x86_64/src/net/door/ip4/tcp.rs
+++ runtime/linux/x86_64/src/net/door/ip4/tcp.rs
@@ -1,5 +1,5 @@
 use crate::{linux, syscall};
-use core::mem;
+use core::mem::{self, MaybeUninit};
 use lemmings_idl_net_ip4_tcp::*;
 
 door! {
@@ -30,17 +30,11 @@ fn connect(x: Connect) -> ConnectResult {
         port,
         timeout,
     } = x;
-    let fd = unsafe { syscall::socket(syscall::AF_INET.into(), syscall::SOCK_STREAM, 0) };
+    let fd = create_socket();
     if fd < 0 {
         todo!("convert error {fd}");
     }
-    let addr = SockAddrIn {
-        family: syscall::AF_INET,
-        port: u16::from(port).to_be(),
-        addr: u32::from(addr).to_be(),
-        _pad: 0,
-    };
-    let res = unsafe { syscall::connect(fd, &addr as *const _ as _, mem::size_of_val(&addr)) };
+    let res = unsafe { apply(fd, addr, port, syscall::connect) };
     if res >= 0 {
         return ConnectOk {
             socket: (fd as u32).into(),
@@ -58,11 +52,50 @@ fn connect(x: Connect) -> ConnectResult {
 }
 
 fn listen(x: Listen) -> ListenResult {
-    todo!();
+    let Listen {
+        addr,
+        port,
+        backlog,
+    } = x;
+    let fd = create_socket();
+    if fd < 0 {
+        todo!("convert error {fd}");
+    }
+    let res = unsafe { apply(fd, addr, port, syscall::bind) };
+    if res >= 0 {
+        let res = unsafe { syscall::listen(fd, backlog.into()) };
+        if res < 0 {
+            todo!("listen error {res}");
+        }
+        return ListenOk {
+            socket: (fd as u32).into(),
+        }
+        .into();
+    }
+    unsafe { syscall::close(fd) };
+    let socket = u32::MAX.into();
+    let error = match res {
+        _ => todo!("convert error {res}"),
+    };
+    ListenFail { socket, error }.into()
 }
 
 fn accept(socket: Listener) -> NewConnection {
-    todo!();
+    let fd = u32::from(socket) as i32;
+    loop {
+        let mut addr = MaybeUninit::<SockAddrIn>::uninit();
+        let mut len = mem::size_of_val(&addr) as u32;
+        let client = unsafe { syscall::accept(fd, addr.as_mut_ptr().cast(), &mut len) };
+        if client < 0 {
+            todo!("accept error {client}")
+        }
+        let addr = unsafe { addr.assume_init_ref() };
+        return NewConnection {
+            socket: (client as u32).into(),
+            addr: u32::from_be(addr.addr).into(),
+            port: u16::from_be(addr.port).into(),
+        };
+    }
 }
 
 fn recv(x: Recv) -> RecvResult {
@@ -104,9 +137,24 @@ fn shutdown(x: Shutdown) -> ShutdownResult {
 }
 
 fn close_listener(socket: Listener) {
-    todo!();
+    unsafe { syscall::close(u32::from(socket) as i32) };
 }
 
 fn close_connection(socket: Connection) {
-    todo!();
+    unsafe { syscall::close(u32::from(socket) as i32) };
+}
+
+fn create_socket() -> i32 {
+    unsafe { syscall::socket(syscall::AF_INET.into(), syscall::SOCK_STREAM, 0) }
+}
+
+unsafe fn apply(fd: i32, addr: Addr, port: Port, f: unsafe fn(i32, *const (), u32) -> i32) -> i32 {
+    let addr = SockAddrIn {
+        family: syscall::AF_INET,
+        port: u16::from(port).to_be(),
+        addr: u32::from(addr).to_be(),
+        _pad: 0,
+    };
+    let len = mem::size_of_val(&addr) as u32;
+    unsafe { (f)(fd, &addr as *const _ as _, len) }
 }
