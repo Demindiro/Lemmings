a087d6dc3816e07be238ba61b27e1905e8940736
David Hoppenbrouwers <lemmings@demindiro.com>
2025-12-06 01:33:42 +0100

    linux-run.sh: use exec for easier strace/gdb

--- linux-run.sh
+++ linux-run.sh
@@ -4,4 +4,4 @@
 
 set -xe
 cd /tmp/data.bin.dir
-"$RUST_OUT"/x86_64-unknown-linux-none/release/lemmings-runtime-linux-x86_64 "$@"
+exec "$RUST_OUT"/x86_64-unknown-linux-none/release/lemmings-runtime-linux-x86_64 "$@"
