756d2a4a5f05075352e2b9f39b15d729975503ef
David Hoppenbrouwers <lemmings@demindiro.com>
2025-12-29 21:41:05 +0100

    build.sh: add service/net

--- build.sh
+++ build.sh
@@ -19,9 +19,12 @@ make -C IDL
 (cd kernel && ./build.sh)
 (cd interpreter && ./build.sh)
 (cd driver/virtio-net && cargo b --release -Zbuild-std=core)
+(cd service/net && cargo b --release -Zbuild-std=core)
 
 cp interpreter/example.interpreter "$databin/interpreter.init"
 mkdir -p "$databin/driver"
+mkdir -p "$databin/service"
 cp "$RUST_TARGET/release/lemmings-driver-virtio-net" "$databin/driver/virtio-net"
+cp "$RUST_TARGET/release/lemmings-service-net" "$databin/service/net"
 
 ./create_archive.py "$out/data.bin" "$databin"
